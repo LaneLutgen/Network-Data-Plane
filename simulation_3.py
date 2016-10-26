63f1bb596afccd1813e85b9395cec44193546ea7
Fenil Patel <fenil_patel93@ymail.com>
2016-10-25 21:44:36 -0600

    Add files via upload
    Message reaches router C and then drops packet

--- simulation_3.py
+++ simulation_3.py
@@ -3,8 +3,8 @@ Created on Oct 12, 2016
 
 @author: mwitt_000
 '''
-import network_3
-import link_3
+import network
+import link
 import threading
 from time import sleep
 
@@ -14,40 +14,35 @@ simulation_time = 1 #give the network sufficient time to transfer all packets be
 
 if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads
-
-    routa = [0, 1] #These are what I used for routing tables, pretty basic, not sure if he wants more than this.
-    routb = [0]
-    routc = [0]
-    routd = [0]
     
     #create network nodes
-    client = network_3.Host(1)
+    client = network.Host(1)
     object_L.append(client)
-    client1 = network_3.Host(2)
+    client1 = network.Host(2)
     object_L.append(client1)
-    server = network_3.Host(3)
+    server = network.Host(3)
     object_L.append(server)
-    router_a = network_3.Router(name='A', intf_count=2, max_queue_size=router_queue_size, rout=routa)
+    router_a = network.Router(name='A', intf_count=2, max_queue_size=router_queue_size)
     object_L.append(router_a)
-    router_b = network_3.Router(name='B', intf_count=1, max_queue_size=router_queue_size, rout=routb)
+    router_b = network.Router(name='B', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_b)
-    router_c = network_3.Router(name='C', intf_count=1, max_queue_size=router_queue_size, rout=routc)
+    router_c = network.Router(name='C', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_c)
-    router_d = network_3.Router(name='D', intf_count=2, max_queue_size=router_queue_size, rout=routd)
+    router_d = network.Router(name='D', intf_count=2, max_queue_size=router_queue_size)
     object_L.append(router_d)
     
     #create a Link Layer to keep track of links between network nodes
-    link_layer = link_3.LinkLayer()
+    link_layer = link.LinkLayer()
     object_L.append(link_layer)
     
     #add all the links
-    link_layer.add_link(link_3.Link(client, 0, router_a, 0, 50))
-    link_layer.add_link(link_3.Link(client1, 0, router_a, 1, 50))
-    link_layer.add_link(link_3.Link(router_a, 0, router_b, 0, 50))
-    link_layer.add_link(link_3.Link(router_a, 1, router_c, 0, 50))
-    link_layer.add_link(link_3.Link(router_b, 0, router_d, 0, 50))
-    link_layer.add_link(link_3.Link(router_c, 0, router_d, 1, 50))
-    link_layer.add_link(link_3.Link(router_d, 0, server, 0, 50))
+    link_layer.add_link(link.Link(client, 0, router_a, 0, 50))
+    link_layer.add_link(link.Link(client1, 0, router_a, 1, 50))
+    link_layer.add_link(link.Link(router_a, 0, router_b, 0, 50))
+    link_layer.add_link(link.Link(router_a, 1, router_c, 0, 50))
+    link_layer.add_link(link.Link(router_b, 0, router_d, 0, 50))
+    link_layer.add_link(link.Link(router_c, 0, router_d, 1, 50))
+    link_layer.add_link(link.Link(router_d, 0, server, 0, 50))
     
     
     #start all the objects
@@ -65,10 +60,12 @@ if __name__ == '__main__':
     for t in thread_L:
         t.start()
     
+    
     #create some send events    
-    client.udt_send(3, 1, 'Message through router B')
-    client1.udt_send(3, 2, 'Message through router C')
-
+    client.udt_send(3, 'Message through router B')
+    client.udt_send(3, 'Message through router C')
+    
+    
     #give the network sufficient time to transfer all packets before quitting
     sleep(simulation_time)
     
